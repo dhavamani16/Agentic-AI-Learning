a97abc51a0aa3d5f7e4fec233f689db81830b359
Dhavamani A <dhavamani17092005@gmail.com>
2026-03-30 16:07:51 +0530

    Add sentiment analysis script using Hugging Face Transformers pipeline

---
+++ sentiment_analysis.py
@@ -0,0 +1,5 @@
+from transformers import pipeline
+classifier=pipeline("sentiment-analysis",model="distilbert-base-uncased-finetuned-sst-2-english")
+text=input("Enter text")
+result=classifier(text)
+print(result)
